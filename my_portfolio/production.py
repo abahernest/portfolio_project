7c8f1993f706ad6a3095153c05bf6a6f405bca5c
abahjnr <abahernesto@gmail.com>
2020-03-14 07:08:39 -0400

    the last

--- my_portfolio/production.py
+++ my_portfolio/production.py
@@ -1,9 +1,10 @@
 from my_portfolio.common import *
 
-ALLOWED_HOSTS = ['8.0.0.0', 'localhost']
-
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'e^9s8q1bp*n$g@wrs46)0#!*g4@2dw1d8v=*=tu_iv-483h=^u'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+
+# SECURITY WARNING: update this when you have the production host
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost']
--- my_portfolio/settings.py
+++ my_portfolio/settings.py
@@ -1,10 +1,9 @@
 from my_portfolio.common import *
 
+ALLOWED_HOSTS = ['8.0.0.0', 'localhost']
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = 'e^9s8q1bp*n$g@wrs46)0#!*g4@2dw1d8v=*=tu_iv-483h=^u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-
-# SECURITY WARNING: update this when you have the production host
-ALLOWED_HOSTS = ['0.0.0.0', 'localhost']
+DEBUG = True
