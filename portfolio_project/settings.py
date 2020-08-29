e1d7c71423e707cc15e0778ee57a526b49d93ceb
abahernest <32009497+abahernest@users.noreply.github.com>
2020-08-29 14:10:27 +0100

    added favicon

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'l(y#3g&xta*&q=8(yf+po-lr8cg3i3l(so3#e*6!i-u+d3go8-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost','abahernest-portfolio.herokuapp.com']
 
